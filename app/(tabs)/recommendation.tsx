7f4de7f1e9d8a72d1e445447c70e202556a9e43c
davidfoo007 <tech@myunilah.com>
2025-02-15 16:34:00 +0800

    created recommendation page

---
+++ app/(tabs)/recommendation.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const recommendation = () => {
+    return (
+        <View>
+            <Text>recommendation</Text>
+        </View>
+    );
+};
+
+export default recommendation;
